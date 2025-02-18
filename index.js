d5f93f6097a9ea759d246ce4b458771aaa35c171
Meenakshi <meenakshi.v36@gmail.com>
2025-02-18 15:50:26 +0530

    refactor: comment out static file serving and routing in Express app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path');
+//const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -23,11 +23,11 @@ const userRoutes = require('./routes/users');
 app.use(authRoutes);
 app.use(userRoutes);
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+//app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
+//app.get('*', (req, res) => {
+   // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  //});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
